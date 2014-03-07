ddf4be7498177808fa1636efb56e11bd2d3f5ef7
Hack Reactor Students <rob@robertwholmes.com>
2014-03-07 12:11:05 -0800

    Created basic gameboard html/css set up

--- watchout.js
+++ watchout.js
@@ -1,2 +1,27 @@
 // start slingin' some d3 here.
 
+// NOTES
+//
+//  enter() -- creates nodes for incoming data
+//  update --
+//  exit() -- removes nodes (outgoing nodes)
+//  key() --
+
+
+var width = 700;
+var height = 450;
+
+var gameboard = d3.select(".container").append("svg:svg")
+  .attr("width", width)
+  .attr("height", height);
+
+var axes;
+var updateScore = function() {
+};
+var updateTopScore = function() {
+};
+var updateCollisions = functoin() {
+};
+
+
+
