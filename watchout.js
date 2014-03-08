b7b0c15ed6d26b82b740e2d48f77ea0e28550832
Hack Reactor Students <rob@robertwholmes.com>
2014-03-07 17:12:40 -0800

    Made draggable player

--- watchout.js
+++ watchout.js
@@ -2,7 +2,7 @@ var width = 700;
 var height = 450;
 var gameSpeed = 3000;
 
-var enemyColors = ['red', 'blue', 'green', 'purple', 'orange'];
+var enemyColors = ["black"];
 
 var gameboard = d3.select(".container").append("svg")
   .attr("width", width)
@@ -24,7 +24,32 @@ var createEnemies = function(numberOfEmemies) {
   return enemies;
 };
 
+var Player =  function() {
+  this.x = 0;
+  this.y = 0;
+  this.color = "orange";
+};
+
+var player = new Player();
 var enemies = createEnemies(30);
+
+var move = function() {
+  d3.select(this)
+  .attr("transform", "translate(" + d3.event.x + "," + d3.event.y  + ")");
+};
+
+var playerCircles = gameboard.selectAll("circle")
+  .data([player])
+  .enter()
+  .append("circle");
+
+var pcircleAttributes = playerCircles
+  .attr("cx", function (d) { return d.x; })
+  .attr("cy", function (d) { return d.y; })
+  .attr("r", 10)
+  .attr("class", "player")
+  .style("fill", function(d){return d.color;})
+  .call(d3.behavior.drag().on("drag", move));
 
 var enemyCircles = gameboard.selectAll("circle")
   .data(enemies)
@@ -35,10 +60,11 @@ var circleAttributes = enemyCircles
   .attr("cx", function (d) { return d.x; })
   .attr("cy", function (d) { return d.y; })
   .attr("r", 10)
+  .attr("class", "enemy")
   .style("fill", function(d){return d.color;});
 
 window.setInterval(function(){
-  d3.selectAll("circle").transition()
+  d3.selectAll(".enemy").transition()
     .duration(gameSpeed)
     .attr("cx", function(d){
       return setXPos();
